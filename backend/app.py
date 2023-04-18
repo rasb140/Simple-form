3a7d0e08735a44f5ab03ebb5cd405b9a02e95bbb
Rodrigo Borges <rasb140@hotmail.com>
2023-04-18 11:01:35 +0200

    Database Insertion

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,44 @@
+import os
+
 from flask import Flask, request,jsonify,json
 from flask_cors import CORS, cross_origin
 from datetime import date, datetime
 from flask_swagger_ui import get_swaggerui_blueprint
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+print(os.path.join(basedir, 'database.db'))
+
+
+db = SQLAlchemy(app)
+
 SWAGGER_URL = '/api/docs'
 API_URL = '/static/swagger.json'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    dob = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+
+    @staticmethod
+    def age(dob):
+        today = date.today()
+        age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+        return age
+
+    def __repr__(self):
+        return f'<User name:{self.name} dob:{self.dob} age:{User.age(self.dob)}>'
+
+
 
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
@@ -37,6 +68,9 @@ def submitData():
         print(dob)
         print("age")
         print(calculate_age(datetime.fromisoformat(dob)))
+        user_to_add = User(name = name, dob = datetime.fromisoformat(dob))
+        db.session.add(user_to_add)
+        db.session.commit()
 
         response_object['age'] = calculate_age(datetime.fromisoformat(dob))
         return jsonify(response_object)
